75c43931152070ef26c4318d42637d75194993ce
Andrew Sauber <a3sauber@gmail.com>
2016-07-15 20:37:11 -0400

    storing sessions per origin, header string to object, add Makefile

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ var _ = require("underscore");
 
 var hashAlgoMask = btoa("\x01\x05");
 
-function origin(url) {
+function getOrigin(url) {
     // capture everything up to the first lone slash
-    // this includes the scheme, host, and port
-    return url.match(/(.+[^/])\/([^/]|$)/)[1];
+    // including the scheme, host, and port
+    return url.match(/(.+\/\/[^/]+)\/([^/]|$)/)[1];
 }
 
 function domainHasSession(url) {
-    return localStorage[origin(url)] !== undefined;
-};
+    return localStorage[getOrigin(url)] !== undefined;
+}
 
 function objToHeaderString(obj) {
     return _.map(_.keys(obj), function (key) {
@@ -18,30 +18,80 @@ function objToHeaderString(obj) {
     }).join(';');
 }
 
-function genSignedHeader(details) {
-    return "";
-};
-
-function genReadyHeader(details) {
-    var headerValues = objToHeaderString({
-        'r': hashAlgoMask
+function headerStringToObj(str) {
+    if (!str) return {};
+    var pairs = str.split(';');
+    var headerValues = {};
+    _.each(pairs, function(pair) {
+        pair = pair.split(':');
+        headerValues[pair[0]] = atob(pair[1]);
     });
     return headerValues;
-};
+}
+
+function genSignedHeader(details) {
+    domainValues = headerStringToObj(localStorage[getOrigin(details.url)]);
+    return objToHeaderString(domainValues);
+}
+
+function genReadyHeader() {
+    var headerValue = objToHeaderString({
+        'r': hashAlgoMask
+    });
+    return headerValue;
+}
+
+function storeNewSession(url, headerValues) {
+    var origin = getOrigin(url);
+    if (!origin.startsWith("https")) {
+        console.log("Won't store SessionArmor session delivered insecurely.");
+        return;
+    }
+    // use the header string format for internal serialization
+    localStorage[origin] = objToHeaderString(headerValues);
+}
+
+function invalidateSession(url) {
+    var origin = getOrigin(url);
+    var originValues = headerStringToObj(localStorage[origin]);
+    hmacKey = originValues['Kh']
+}
+
+function onHeaderReceived(details) {
+    var headerValues = {};
+    _.each(details.responseHeaders, function(header) {
+        if (header.name !== "X-S-Armor") return;
+        headerValues = headerStringToObj(header.value);
+    });
+
+    if (headerValues.hasOwnProperty('s')) {
+        storeNewSession(details.url, headerValues);
+    } else if (headerValues.hasOwnProperty('i')) {
+        invalidateSession(details.url);
+    }
+    console.log(headerValues, details);
+}
+
+function beforeSendHeader(details) {
+    var headerValue = 
+        domainHasSession(details.url)
+            ? genSignedHeader(details)
+            : genReadyHeader();
+    details.requestHeaders.push({
+        "name": "X-S-Armor",
+        "value": headerValue 
+    });
+    return {requestHeaders: details.requestHeaders};
+}
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-        var headerValue = domainHasSession(details.url) ? 
-            genSignedHeader(details) :
-            genReadyHeader(details);
-        details.requestHeaders.push({
-            "name": "X-S-Armor",
-            "value": headerValue 
-        });
-        return {requestHeaders: details.requestHeaders};
-    },
+    beforeSendHeader,
     {"urls": ["https://*/*", "http://*/*"]},
     ["blocking", "requestHeaders"]
 );
 
-
+chrome.webRequest.onHeadersReceived.addListener(
+    onHeaderReceived,
+    {"urls": ["https://*/*", "http://*/*"]},
+    ["blocking", "responseHeaders"]
+);
